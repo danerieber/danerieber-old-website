fc6fc36241d2ae67727b19eb09821a9c27c70098
Dane Rieber <danerieber@gmail.com>
2018-08-27 08:46:33 -0600

    Created Home and About pages

---
+++ js/index.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    navigation: true,
+    navigationTooltips: ['Home', 'About']
+});
+
+new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 20
+});

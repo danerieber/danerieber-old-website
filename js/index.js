c217557fb8ab0f6c74f5e7da6f0bdbbf3ea1d175
Dane Rieber <danerieber@gmail.com>
2018-08-28 09:25:45 -0600

    Added projects page, animations on scroll, and worked on mobile layout

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 new fullpage('#fullpage', {
     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
     navigation: true,
-    navigationTooltips: ['Home', 'About']
+    navigationTooltips: ['Home', 'About', 'Projects'],
+    scrollBar: true
 });
 
 new Typed("#typed", {
     stringsElement: '#typed-strings',
     typeSpeed: 20
 });
+
+new WOW().init();

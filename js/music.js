54281ba2e4f9a65f2af596b88bae76f4f7089efe
Dane Rieber <danerieber@gmail.com>
2018-09-06 16:25:15 -0600

    Fixed d-logo not switching on theme change after layout changes

--- js/music.js
+++ js/music.js
@@ -27,12 +27,12 @@ function toggleTheme() {
     if (canChangeTheme) {
         if (!darkTheme) {
             $('head').append('<link id="music-dark" rel="stylesheet" href="../css/music-dark.css">');
-            $('.d-logo').attr('src', '../svg/d-logo-white.svg');
+            $('.d-logo-img').attr('src', '../svg/d-logo-white.svg');
             $('.btn-outline-dark').addClass('btn-outline-light');
             $('.btn-outline-dark').removeClass('btn-outline-dark');
         } else {
             $('#music-dark').remove();
-            $('.d-logo').attr('src', '../svg/d-logo.svg');
+            $('.d-logo-img').attr('src', '../svg/d-logo.svg');
             $('.btn-outline-light').addClass('btn-outline-dark');
             $('.btn-outline-light').removeClass('btn-outline-light');
         }
